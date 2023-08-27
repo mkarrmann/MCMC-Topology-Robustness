56915cc06a365518a84ecd83b557ad2d4b35a0e4
Matt Karrmann <mattkarrmann@gmail.com>
2023-08-27 13:15:40 -0500

    Log info

--- coarseness/random_contraction.py
+++ coarseness/random_contraction.py
@@ -49,6 +49,11 @@ ELECTION_MAP = {
 CONFIG: dict = {}
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 # Copied from tree.py with fixes.
